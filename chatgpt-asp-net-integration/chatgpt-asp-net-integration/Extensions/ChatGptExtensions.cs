37429eb46d8edee96a103d3b85a7256009810d25
leandrocgsi <leandrocgsi@gmail.com>
2023-11-07 18:07:26 -0300

    Initial commit

--- chatgpt-asp-net-integration/chatgpt-asp-net-integration/Extensions/ChatGptExtensions.cs
+++ chatgpt-asp-net-integration/chatgpt-asp-net-integration/Extensions/ChatGptExtensions.cs
@@ -6,7 +6,8 @@ public static class ChatGptExtensions
 {
 	public static WebApplicationBuilder AddChatGpt(this WebApplicationBuilder builder, IConfiguration configuration)
 	{
-		var key = configuration["ChatGpt:Key"];
+		// var key = configuration["ChatGpt:Key"];
+		var key = Environment.GetEnvironmentVariable("OPENAI_API_KEY");
 
 		var chat = new OpenAIAPI(key);
 
