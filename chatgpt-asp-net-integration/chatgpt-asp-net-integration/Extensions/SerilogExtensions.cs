18d7edd6e99fb60ed6af4323cce689fa828cc8be
leandrocgsi <leandrocgsi@gmail.com>
2023-11-07 17:58:50 -0300

    Initial commit

--- chatgpt-asp-net-integration/chatgpt-asp-net-integration/Extensions/SerilogExtensions.cs
+++ chatgpt-asp-net-integration/chatgpt-asp-net-integration/Extensions/SerilogExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using Serilog;
+using Serilog.Configuration;
 
 namespace ChatGPT.ASP.NET.Integration.Extensions;
 
@@ -6,9 +7,7 @@ public static class SerilogExtensions
 {
 	public static WebApplicationBuilder AddSerilog(this WebApplicationBuilder builder, IConfiguration configuration, string applicationName)
 	{
-		Log.Logger = new LoggerConfiguration().ReadFrom.Configuration(configuration).Enrich.FromLogContext().Enrich.WithMachineName().Enrich.WithEnvironmentUserName().Enrich
-			.WithExceptionDetails().Enrich.WithProperty("ApplicationName", $"{applicationName} - {configuration.GetSection("DOTNET_ENVIRONMENT")?.Value}").WriteTo
-			.Async(writeTo => writeTo.Console(outputTemplate: "[{Timestamp:HH:mm:ss} {Level:u3}] {Message:lj} {Properties:j}{NewLine}{Exception}")).CreateLogger();
+		Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
 
 		builder.Logging.ClearProviders();
 		builder.Host.UseSerilog(Log.Logger, true);
